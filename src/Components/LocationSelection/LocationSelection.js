dd2ac7a284e1955a2ae11352d4efd85d22748a81
Justin Anthony <justincanthony@gmail.com>
2021-10-15 16:05:16 -0600

    Create handleChange function

--- src/Components/LocationSelection/LocationSelection.js
+++ src/Components/LocationSelection/LocationSelection.js
@@ -4,11 +4,23 @@ import './LocationSelection.css';
 export const LocationSelection = () => {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [error, setError] = useState('');
+
+  const handleClick = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  };
 
   return (
     <div className="location-selection">
       <h4>Click The Button To Get Stories Near You</h4>
-      <button className="get-stories-btn" type="click" onClick={handleClick}>
+      <button
+        className="get-stories-btn"
+        type="click"
+        onClick={(e) => handleClick(e)}
+      >
         Get Stories
       </button>
     </div>
