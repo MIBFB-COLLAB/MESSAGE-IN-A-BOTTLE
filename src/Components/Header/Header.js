3c4a07283b2a888348b8ecdf9655f5b59cce7043
Fara Akhatova <faraakhatova@gmail.com>
2021-10-15 15:43:31 -0600

    Add NavBar component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,10 @@
 import './Header.css';
 import React from "react"
 
-
-
 const Header = () => {
     return (
         <header>
-            <nav>
-                <h1>Message in a bottle</h1>
-                <button className='story-submit'>Submit a story</button>
-            </nav>
+            <h1>Message in a bottle</h1>
         </header>
     )
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return (
+        <nav>   
+            <button className='story-submit'>Submit a story</button>
+        </nav>
+    )
+    
+}
+
+export default NavBar;
