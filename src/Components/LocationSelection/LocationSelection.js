9b4f47c97eeef446fa2a7c3d34ed93f15450253a
Justin Anthony <justincanthony@gmail.com>
2021-10-15 16:00:10 -0600

    Add html elems

--- src/Components/LocationSelection/LocationSelection.js
+++ src/Components/LocationSelection/LocationSelection.js
@@ -5,5 +5,12 @@ export const LocationSelection = () => {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
-  return <div></div>;
+  return (
+    <div className="location-selection">
+      <h4>Click The Button To Get Stories Near You</h4>
+      <button className="get-stories-btn" type="click" onClick={handleClick}>
+        Get Stories
+      </button>
+    </div>
+  );
 };
