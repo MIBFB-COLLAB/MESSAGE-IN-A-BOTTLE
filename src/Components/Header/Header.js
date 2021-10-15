04ed38c13ad6bc43c34de9b657203d275c81c384
Fara Akhatova <faraakhatova@gmail.com>
2021-10-15 15:32:01 -0600

    add classname for submit a story button in Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <header>
             <nav>
                 <h1>Message in a bottle</h1>
-                <button>Submit a story</button>
+                <button className='story-submit'>Submit a story</button>
             </nav>
         </header>
     )
