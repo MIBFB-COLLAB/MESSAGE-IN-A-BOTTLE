bdfe36e639228b526eb7667d93c8ba20c0fc8e1d
Mae Duphorne <maelduphorne@gmail.com>
2021-10-15 17:52:49 -0600

    add stories page shell

--- src/Components/StoriesPage/StoriesPage.js
+++ src/Components/StoriesPage/StoriesPage.js
@@ -1,2 +1,8 @@
 import './StoriesPage.css';
 import React, { useEffect } from "react"
+
+const StoriesPage = () => {
+
+}
+
+export default StoriesPage;                                                                              
