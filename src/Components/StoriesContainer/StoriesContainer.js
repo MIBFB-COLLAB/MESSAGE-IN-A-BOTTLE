4d21bd6317192c48333742b78f653929786677c9
Mae Duphorne <maelduphorne@gmail.com>
2021-10-15 17:46:20 -0600

    add useEffect note and storyCards method

--- src/Components/StoriesContainer/StoriesContainer.js
+++ src/Components/StoriesContainer/StoriesContainer.js
@@ -1,14 +1,29 @@
 import './StoriesContainer.css';
 import React, { useEffect } from "react"
 
-const StoriesContainer = (props) => {
+const StoriesContainer = ({ longitude, latitude }) => {
+  const [currentStories, setCurrentStories] = useState([])
+
+  // useEffect(() => {
+  //   run fetch call of current location using longitude and latitude props
+  //   set state of currentStories
+  // }, [])
+
+  const storyCards = currentStories.map((story) => {
+    return (
+      <StoryCard
+        // key={uuid}
+      />
+    )
+  })
+
   return (
-    <div>
+    <section className="stories-container">
       <h2>These are the Story Cards</h2>
-      <section>
+      <div>
         { storyCards }
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
