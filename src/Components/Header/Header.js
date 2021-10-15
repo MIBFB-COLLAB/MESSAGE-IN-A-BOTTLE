d5808771729ee67e5a375a2fac47dcb0d8ffd187
Fara Akhatova <faraakhatova@gmail.com>
2021-10-15 15:31:08 -0600

    addheader components JSX elements

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,2 +1,15 @@
 import './Header.css';
 import React from "react"
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <h1>Message in a bottle</h1>
+                <button>Submit a story</button>
+            </nav>
+        </header>
+    )
+}
