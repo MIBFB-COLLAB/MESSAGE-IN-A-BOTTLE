0649638f2e64eedb0f62ff0d28a9d2090c5f5c23
Fara Akhatova <faraakhatova@gmail.com>
2021-10-16 10:27:51 -0600

    add chnages

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import React from "react"
 const Header = () => {
     return (
         <header className='Header'>
-            <h1>Message in a bottle</h1>
+            <h1> Welcome to Message in a Bottle </h1>
         </header>
     )
 }
