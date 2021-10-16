849c0054ec16fd0a00640f5f13a356b528dded25
Mae Duphorne <maelduphorne@gmail.com>
2021-10-16 12:19:18 -0600

    set jsx shell for error card

--- src/Components/ErrorHandlingCard/ErrorHandlingCard.js
+++ src/Components/ErrorHandlingCard/ErrorHandlingCard.js
@@ -1,2 +1,13 @@
 import './ErrorHandlingCard.css';
 import React from "react"
+
+const ErrorHandlingCard = () => {
+  return (
+    <div className="error-card">
+      <p>OOPS! You're not within range of this story right now</p>
+      <button className="directions-button">BACK TO DIRECTIONS</button>
+    </div>
+  )
+};
+
+export default ErrorHandlingCard;
