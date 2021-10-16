0006dc4e1d32ed5444584a255996a0ba68f3075c
Mae Duphorne <maelduphorne@gmail.com>
2021-10-15 18:01:28 -0600

    Add stories Page components

--- src/Components/StoriesPage/StoriesPage.js
+++ src/Components/StoriesPage/StoriesPage.js
@@ -1,8 +1,15 @@
 import './StoriesPage.css';
 import React, { useEffect } from "react"
+import StoriesContainer from "../StoriesContainer/StoriesContainer";
 
 const StoriesPage = () => {
-
+  return(
+    <div>
+      <h1>Stories Page Header component goes here</h1>
+      <h2>User Instructions component goes here</h2>
+      <StoriesContainer/>
+    </div>
+  )
 }
 
 export default StoriesPage;                                                                              
