b7d7f304bc29363c3b510f02eff48a3d568bbbaf
Mae Duphorne <maelduphorne@gmail.com>
2021-10-15 18:09:30 -0600

    add test data as currentStories state

--- src/Components/StoriesContainer/StoriesContainer.js
+++ src/Components/StoriesContainer/StoriesContainer.js
@@ -1,8 +1,32 @@
 import './StoriesContainer.css';
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react";
+import StoryCard from "../StoryCard/StoryCard";
 
 const StoriesContainer = ({ longitude, latitude }) => {
-  const [currentStories, setCurrentStories] = useState([])
+  const [currentStories, setCurrentStories] = useState([
+    {
+      "id": 1,
+      "type": "story",
+      "attributes": {
+        "title": "my cool story",
+        "message": "This one time I saw a bird",
+        "latitude": 13.201,
+        "longitude": 9.2673,
+        "distance_in_miles": 1.2
+        }
+      },
+      {
+      "id": 2,
+      "type": "another story",
+      "attributes": {
+        "title": "my cool second story",
+        "message": "This one time I saw another bird",
+        "latitude": 13.201,
+        "longitude": 9.2673,
+        "distance_in_miles": 1.5
+        }
+      }
+  ])
 
   // useEffect(() => {
   //   run fetch call of current location using longitude and latitude props
@@ -13,6 +37,8 @@ const StoriesContainer = ({ longitude, latitude }) => {
     return (
       <StoryCard
         // key={uuid}
+        title={story.atrributes.title}
+        distance={story.attributes.distance_in_miles}
       />
     )
   })
