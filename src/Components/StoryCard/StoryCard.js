a3308d7029ef21b241bd2e0a840509fa09235a38
Mae Duphorne <maelduphorne@gmail.com>
2021-10-15 18:13:12 -0600

    pass props and display cards

--- src/Components/StoryCard/StoryCard.js
+++ src/Components/StoryCard/StoryCard.js
@@ -1,8 +1,16 @@
 import './StoryCard.css';
 import React from "react"
 
-const StoryCard = () => {
-  
+const StoryCard = ({title, distance}) => {
+  return(
+    <article>
+      <h3>{title}</h3>
+      <p>Distance from story: {distance}</p>
+      <button className="get-directions-btn">GET DIRECTIONS</button>
+      <button className="view-story-btn">VIEW STORY</button>
+    </article>
+  )
+
 }
 
 export default StoryCard;
