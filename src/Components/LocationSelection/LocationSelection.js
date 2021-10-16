e19aceedd3d95af8b80e7d2454cc3f4de2a52a94
Justin Anthony <justincanthony@gmail.com>
2021-10-16 14:18:30 -0600

    Add functions to retrieve location

--- src/Components/LocationSelection/LocationSelection.js
+++ src/Components/LocationSelection/LocationSelection.js
@@ -6,23 +6,37 @@ export const LocationSelection = () => {
   const [longitude, setLongitude] = useState('');
   const [error, setError] = useState('');
 
+  const getLocation = (position) => {
+    console.log(position);
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
+
+  const catchError = () => {
+    setError('Sorry, no position available.');
+    console.log(error);
+  };
+
   const handleClick = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
+    navigator.geolocation.getCurrentPosition(getLocation, catchError);
   };
 
   return (
-    <div className="location-selection">
-      <h4>Click The Button To Get Stories Near You</h4>
-      <button
-        className="get-stories-btn"
-        type="click"
-        onClick={(e) => handleClick(e)}
-      >
-        Get Stories
-      </button>
-    </div>
+    <>
+      {error && <p>{error}</p>}
+      {!error && (
+        <div className="location-selection">
+          <h4>Click The Button To Get Stories Near You</h4>
+
+          <button
+            className="get-stories-btn"
+            type="click"
+            onClick={(e) => handleClick(e)}
+          >
+            Get Stories
+          </button>
+        </div>
+      )}
+    </>
   );
 };
