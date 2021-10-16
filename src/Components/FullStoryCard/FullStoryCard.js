3c979e1614c414ec10ebbcfcb0f076825c65b126
Mae Duphorne <maelduphorne@gmail.com>
2021-10-16 12:24:50 -0600

    set jsx shell for full story card

--- src/Components/FullStoryCard/FullStoryCard.js
+++ src/Components/FullStoryCard/FullStoryCard.js
@@ -1,2 +1,14 @@
 import './FullStoryCard.css';
 import React from "react"
+
+const FullStoryCard = ({title, story}) => {
+  return (
+    <article className="full-story-card">
+      <h3>{title}</h3>
+      <p>{story}</p>
+      <button>BACK TO CARD</button>
+    </article>
+  )
+}
+
+export default FullStoryCard;
