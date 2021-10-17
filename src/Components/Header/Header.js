dbc5ee63200bac6b1a3e74a16bac4717667e0950
Justin Anthony <justincanthony@gmail.com>
2021-10-17 15:23:50 -0600

    Move submit story button to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,13 @@
 import './Header.css';
-import React from "react"
+import React from 'react';
 
 const Header = () => {
-    return (
-        <header className='Header'>
-            <h1> Welcome to Message in a Bottle </h1>
-        </header>
-    )
-}
-
+  return (
+    <header className="Header">
+      <h1> Welcome to Message in a Bottle </h1>
+      <button className="story-submit">Submit a story</button>
+    </header>
+  );
+};
 
 export default Header;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,13 @@
-import {useState} from 'react'
-import './NavBar.css'
+// import { useState } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
-    const [newStory, setNewStory] = useState(false)
-    return (
-        <nav className='Nav-bar'> 
-        <button className='story-submit' onClick = {(e) => setNewStory(true)}>Submit a story</button>
-        </nav>
-    )
-    
-}
+  //   const [newStory, setNewStory] = useState(false);
+  return (
+    <nav className="Nav-bar">
+      {/* <button className='story-submit' onClick = {(e) => setNewStory(true)}>Submit a story</button> */}
+    </nav>
+  );
+};
 
 export default NavBar;
