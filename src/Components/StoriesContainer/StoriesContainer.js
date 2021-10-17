64235c7a1b70e239ec5abca4b60a7336fe67255e
Justin Anthony <justincanthony@gmail.com>
2021-10-17 14:09:37 -0600

    Remove test data

--- src/Components/StoriesContainer/StoriesContainer.js
+++ src/Components/StoriesContainer/StoriesContainer.js
@@ -6,36 +6,13 @@ import { getData } from '../../apiCalls';
 const { v4: uuidv4 } = require('uuid');
 
 const StoriesContainer = ({ longitude, latitude }) => {
-  const [currentStories, setCurrentStories] = useState([
-    {
-      id: 1,
-      type: 'story',
-      attributes: {
-        title: 'my cool story',
-        message: 'This one time I saw a bird',
-        latitude: 13.201,
-        longitude: 9.2673,
-        distance_in_miles: 1.2,
-      },
-    },
-    {
-      id: 2,
-      type: 'another story',
-      attributes: {
-        title: 'my cool second story',
-        message: 'This one time I saw another bird',
-        latitude: 13.201,
-        longitude: 9.2673,
-        distance_in_miles: 1.5,
-      },
-    },
-  ]);
+  const [currentStories, setCurrentStories] = useState([]);
 
-  // We are currently console logging the data, we can set state when the data is ready from the BE
+  
   const getStories = () => {
     getData()
-      // .then((data) => setCurrentStories(data.data))
-      .then((data) => console.log(data));
+      .then((data) => setCurrentStories(data.data))
+      .catch((error) => );
   };
 
   useEffect(() => {
@@ -56,3 +33,31 @@ const StoriesContainer = ({ longitude, latitude }) => {
 };
 
 export default StoriesContainer;
+
+
+
+// Test Data
+// [
+//   {
+//     id: 1,
+//     type: 'story',
+//     attributes: {
+//       title: 'my cool story',
+//       message: 'This one time I saw a bird',
+//       latitude: 13.201,
+//       longitude: 9.2673,
+//       distance_in_miles: 1.2,
+//     },
+//   },
+//   {
+//     id: 2,
+//     type: 'another story',
+//     attributes: {
+//       title: 'my cool second story',
+//       message: 'This one time I saw another bird',
+//       latitude: 13.201,
+//       longitude: 9.2673,
+//       distance_in_miles: 1.5,
+//     },
+//   },
+// ]
