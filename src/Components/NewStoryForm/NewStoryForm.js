18f1638f890a5d7651285c8865c2c65deeb1319e
Justin Anthony <justincanthony@gmail.com>
2021-10-17 15:54:28 -0600

    Add title prop and input

--- src/Components/NewStoryForm/NewStoryForm.js
+++ src/Components/NewStoryForm/NewStoryForm.js
@@ -1,10 +1,18 @@
 import './NewStoryForm.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const NewStoryForm = () => {
+  const [title, setTitle] = useState('');
   return (
-    <div>
-      <p>Hello, It's Me, I'm a Form</p>
-    </div>
+    <form type="submit" className="new-story-form">
+      <input
+        type="text"
+        className="title"
+        placeholder="title"
+        value={title}
+        required
+        onChange={(e) => setTitle(e.target.value)}
+      />
+    </form>
   );
 };
