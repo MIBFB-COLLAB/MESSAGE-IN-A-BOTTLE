10e0d218e6f63a6aa972b903050cd9ddb0afbe50
Justin Anthony <justincanthony@gmail.com>
2021-10-17 15:46:06 -0600

    Create new story form component

--- src/Components/NewStoryForm/NewStoryForm.js
+++ src/Components/NewStoryForm/NewStoryForm.js
@@ -1,2 +1,10 @@
 import './NewStoryForm.css';
-import React from "react"
+import React from 'react';
+
+export const NewStoryForm = () => {
+  return (
+    <div>
+      <p>Hello, It's Me, I'm a Form</p>
+    </div>
+  );
+};
