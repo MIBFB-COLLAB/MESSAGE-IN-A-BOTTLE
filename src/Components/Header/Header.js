5835f2d54b97c70cd6552fb2e0515559b0160d55
Justin Anthony <justincanthony@gmail.com>
2021-10-17 15:45:31 -0600

    Add micromodal to submit story

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,28 @@
 import './Header.css';
 import React from 'react';
+import MicroModal from 'react-micro-modal';
+import { NewStoryForm } from '../NewStoryForm/NewStoryForm';
 
 const Header = () => {
   return (
     <header className="Header">
       <h1> Welcome to Message in a Bottle </h1>
-      <button className="story-submit">Submit a story</button>
+      <MicroModal
+        trigger={(open) => (
+          <div onClick={open}>
+            <button className="story-submit">Submit a story</button>
+          </div>
+        )}
+      >
+        {(close) => {
+          return (
+            <article>
+              <h3>Create Your Message</h3>
+              <NewStoryForm />
+            </article>
+          );
+        }}
+      </MicroModal>
     </header>
   );
 };
