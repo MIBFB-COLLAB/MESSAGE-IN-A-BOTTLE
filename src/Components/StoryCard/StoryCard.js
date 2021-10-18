fc45881f3e11a6903480bf7ca40a7e5d1280ec1f
Mae Duphorne <maelduphorne@gmail.com>
2021-10-18 16:23:48 -0600

    add midromodal wrap to directions button

--- src/Components/StoryCard/StoryCard.js
+++ src/Components/StoryCard/StoryCard.js
@@ -1,12 +1,27 @@
 import './StoryCard.css';
-import React from "react"
+import React from "react";
+import MicroModal from 'react-micro-modal';
+import Directions from '../Directions/Directions';
 
 const StoryCard = ({title, distance}) => {
   return(
     <article className="story-card">
       <h3 className="story-title">{title}</h3>
       <p className="story-distance">Distance from story: {distance}</p>
-      <button className="get-directions-btn">GET DIRECTIONS</button>
+      <MicroModal 
+        trigger={(open) => (
+          <div onClick={open}>
+            <button className="get-directions-btn">GET DIRECTIONS</button>
+          </div>
+      )}>
+        {(close) => {
+          return (
+            <article className="directions-modal">
+              <Directions />
+            </article>
+          )
+        }}
+      </MicroModal>
       <button className="view-story-btn">VIEW STORY</button>
     </article>
   )
