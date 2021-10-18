63b5006335a09e4737c4dfe2d78927bd7c035985
Fara Akhatova <faraakhatova@gmail.com>
2021-10-18 12:23:23 -0600

    Add textarea html element for story to be edited

--- src/Components/StoryEdit/StoryEdit.js
+++ src/Components/StoryEdit/StoryEdit.js
@@ -2,9 +2,18 @@ import './StoryEdit.css';
 import React from "react"
 
 const StoryEdit = () => {
+
     return (
         <form className='edit-story-form'>
-
+            <textarea
+                type="text"
+                className="message"
+                placeholder="type your story here"
+                value=''
+                required
+                
+            />
+            
         </form>
     )
 }
