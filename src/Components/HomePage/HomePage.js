eec25190aeadda1b71ae7a0c5f27b6d7a2cda1c0
Justin Anthony <justincanthony@gmail.com>
2021-10-18 10:53:21 -0600

    Remove navbar component

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,14 +1,12 @@
 import './HomePage.css';
 import React from 'react';
 import Header from '../Header/Header';
-import NavBar from '../NavBar/NavBar';
 import UserInstructions from '../UserInstructions/UserInstructions';
 
 const HomePage = () => {
   return (
     <section className="Home-page">
       <Header />
-      {/* <NavBar/> */}
       <UserInstructions />
     </section>
   );
