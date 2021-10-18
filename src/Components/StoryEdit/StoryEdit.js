0ff56f392a3771bc92d637a7a138230368b2281d
Fara Akhatova <faraakhatova@gmail.com>
2021-10-18 12:20:20 -0600

    Add JSX form element to a StroyEdit component

--- src/Components/StoryEdit/StoryEdit.js
+++ src/Components/StoryEdit/StoryEdit.js
@@ -1,2 +1,12 @@
 import './StoryEdit.css';
 import React from "react"
+
+const StoryEdit = () => {
+    return (
+        <form className='edit-story-form'>
+
+        </form>
+    )
+}
+
+export default StoryEdit;
