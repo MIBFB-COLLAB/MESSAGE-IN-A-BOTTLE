7a56011c3618cc99ce0e99af0820911b953beec0
Justin Anthony <justincanthony@gmail.com>
2021-10-18 18:36:06 -0600

    Add micromodal for viewing story

--- src/Components/StoryCard/StoryCard.js
+++ src/Components/StoryCard/StoryCard.js
@@ -1,58 +1,72 @@
 import './StoryCard.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import MicroModal from 'react-micro-modal';
 import Directions from '../Directions/Directions';
 import ErrorHandlingCard from '../ErrorHandlingCard/ErrorHandlingCard';
 import { getDirections } from '../../apiCalls';
 
-const StoryCard = ({id, title, distance, latitude, longitude}) => {
-  const [directions, setDirections] = useState('')
-  const [error, setError] = useState('')
+const StoryCard = ({ id, title, distance, latitude, longitude }) => {
+  const [directions, setDirections] = useState('');
+  const [error, setError] = useState('');
 
   const handleDirectionsClick = () => {
-    console.log('handlin the click')
+    console.log('handlin the click');
     getDirections(id, latitude, longitude)
-    // We may need to change how we access the data here depending on data structure
-    .then((data) => setDirections)
-    // We may need to change how we access the error message here depending on data structure
-    .catch((error) => setError(error.message))
-  }
+      // We may need to change how we access the data here depending on data structure
+      .then((data) => setDirections)
+      // We may need to change how we access the error message here depending on data structure
+      .catch((error) => setError(error.message));
+  };
+
+  const getStory = () => {};
 
   useEffect(() => {
-    return (
-      <ErrorHandlingCard errorMessage={error}/>
-    )
-  }, [error])
+    return <ErrorHandlingCard errorMessage={error} />;
+  }, [error]);
 
-  return(
+  return (
     <article className="story-card">
       <h3 className="story-title">{title}</h3>
       <p className="story-distance">Distance from story: {distance}</p>
-      <MicroModal 
+      <MicroModal
         trigger={(open) => (
           <div onClick={open}>
-            <button 
-              className="get-directions-btn" 
-              onClick={() => handleDirectionsClick()}>
+            <button
+              className="get-directions-btn"
+              onClick={() => handleDirectionsClick()}
+            >
               GET DIRECTIONS
             </button>
           </div>
-      )}>
+        )}
+      >
         {(close) => {
           return (
             <article className="directions-modal">
-              <Directions 
-                title={title} 
-                directions={directions}
-              />
+              <Directions title={title} directions={directions} />
             </article>
-          )
+          );
         }}
       </MicroModal>
-      <button className="view-story-btn">VIEW STORY</button>
+      <MicroModal
+        trigger={(open) => (
+          <div onClick={open}>
+            <button className="view-story-btn" onClick={() => getStory()}>
+              View Story
+            </button>
+          </div>
+        )}
+      >
+        {(close) => {
+          return (
+            <article className="story-modal">
+              <FullStoryCard title={title} message={message} />
+            </article>
+          );
+        }}
+      </MicroModal>
     </article>
-  )
-
-}
+  );
+};
 
 export default StoryCard;
