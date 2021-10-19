110a9566a7b31362def7f060e425a20b137c8c15
Mae Duphorne <maelduphorne@gmail.com>
2021-10-19 17:23:41 -0600

    Move location selection component to homepage

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import React from 'react';
 import StoriesPage from '../StoriesPage/StoriesPage';
 import HomePage from '../HomePage/HomePage';
-import { LocationSelection } from '../LocationSelection/LocationSelection';
 
 const App = () => {
   return (
     <div className="App">
       <HomePage />
       <StoriesPage />
-      <LocationSelection />
     </div>
   );
 };
