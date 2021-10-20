7e781b31377a1dc8f3a10cec1b06827d773dbc8b
Fara Akhatova <faraakhatova@gmail.com>
2021-10-20 15:04:50 -0600

    Add test for home page url

--- cypress/integration/LandingPage_spec.js
+++ cypress/integration/LandingPage_spec.js
@@ -2,5 +2,9 @@ describe('Landing Page', () => {
   beforeEach('Home Page' , () => {
     cy.visit('http://localhost:3000/')
   });
-  
+
+  it('Should be able to access to application by URL and see Home Page', () => {
+    cy.url().should('include', '/')
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
 });
