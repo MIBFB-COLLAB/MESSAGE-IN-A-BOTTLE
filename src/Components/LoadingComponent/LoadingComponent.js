3801cdb8016db4f08249c516135765ed4a4d6525
Justin Anthony <justincanthony@gmail.com>
2021-10-20 16:22:51 -0600

    Create loading component

---
+++ src/Components/LoadingComponent/LoadingComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './LoadingComponent.css';
+
+export const LoadingComponent = () => {
+  return (
+    <div class="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
