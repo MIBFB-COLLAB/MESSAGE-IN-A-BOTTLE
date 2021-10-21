543f96f35db3a56f4fce29985763ba0bfc992334
Fara Akhatova <faraakhatova@gmail.com>
2021-10-21 11:31:45 -0600

    Add test for Post request and use stubbed intercept

--- cypress/integration/LandingPage_spec.js
+++ cypress/integration/LandingPage_spec.js
@@ -38,42 +38,43 @@ describe('Landing Page', () => {
     })
   });
 
-  // it('Should be able to see button and text to get stories near by', () => {
-  //   cy.get('.location-selection')
-  //   .get('h4').contains('Click The Button To Get Stories Near You')
-  //   .get('.get-stories-btn').contains('Get Stories').click()// need add click after stubbing  http GET request
-  //   cy.GetStory('GET')
-  //   cy.visit('http://localhost:3000/storiesPage')
-  //   cy.url().should('include', '/storiesPage')
-  //   cy.url().should('eq', 'http://localhost:3000/storiesPage')
-  // })
+  it('Should be able to see button and text to get stories near by', () => {
+    cy.get('.location-selection')
+    .get('h4').contains('Click The Button To Get Stories Near You')
+    .get('.get-stories-btn').contains('Get Stories').click()// need add click after stubbing  http GET request
+    cy.GetStory('GET')
+    cy.visit('http://localhost:3000/storiesPage')
+    cy.url().should('include', '/storiesPage')
+    cy.url().should('eq', 'http://localhost:3000/storiesPage')
+  })
   
-  // it('Should be able to get stories near by clicking the button', () => {
-  //   cy.get('.location-selection')
-  //   .get('.get-stories-btn').contains('Get Stories')
-  // })
+  it('Should be able to get stories near by clicking the button', () => {
+    cy.get('.location-selection')
+    .get('.get-stories-btn').contains('Get Stories')
+  })
 });
 
-// describe('Create new stroy', () => {
-//   beforeEach('New card form modal', () => {
-//     cy.visit('http://localhost:3000/')
-//   })
+describe('Create new story', () => {
+  beforeEach('New card form modal', () => {
+    cy.visit('http://localhost:3000/')
+  })
 
-//   it('Should be able click submit story button, in order to submit new story', () => {
-//     cy.get('Header')
-//     .get('div')
-//     .get('.story-submit').contains('Submit a story').click()
-//     cy.on('window:confirm',(txt)=>{
-//       expect(txt).to.contains('Allow', 'Block');
-//     })
+  it('Should be able click submit story button, in order to submit new story', () => {
+    cy.get('Header')
+    .get('div')
+    .get('.story-submit').contains('Submit a story').click()
+    cy.on('window:confirm',(txt)=>{
+      expect(txt).to.contains('Allow', 'Block');
+    })
 
-//     cy.get('article')
-//     .get('h3').contains('Create Your Message')
-//     .get('div')
-//     .get('.new-story-form')
-//     .get('.title').type('Hello')
-//     .get('textarea').type('Hellooooo')
-//     .get('h2').contains('characters left')
-//     .get('button').contains('Submit Story') // need to add click, after stubing http GET request, and invoke stub
-//   });
-// })
+    cy.get('article')
+    .get('h3').contains('Create Your Message')
+    .get('div')
+    .get('.new-story-form')
+    .get('.title').type('Hello')
+    .get('textarea').type('Hellooooo')
+    .get('h2').contains('characters left')
+    .get('button').contains('Submit Story').click()
+    cy.GetStory('POST')
+  });
+})
