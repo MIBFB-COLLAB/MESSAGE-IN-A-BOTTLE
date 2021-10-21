3ddd91f89269ae2b85534c3204bcc729bf0e84ac
Fara Akhatova <faraakhatova@gmail.com>
2021-10-21 10:12:29 -0600

    Add special command for stubbing get http request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,40 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+ const baseURL = 'https://message-in-a-bottle-api.herokuapp.com/api/v1/stories'
+
+Cypress.Commands.add('Get Story', () => {
+    cy.intercept(baseURL, {
+        method: 'GET',
+        statusCode: 200,
+        body: {
+            "data":[
+            {
+                "id":54,
+                "latitude":-18.982791,
+                "longitude":125.456892,
+                "message":"Ich habe meinen Freund im Denver verloren und jetzt bin Ich ganz allein :(",
+                "name":"Till Lindemann",
+                "title":"Wie Traurig",
+                "location":"",
+                "created_at":"2021-10-20T23:14:32.817628Z",
+                "updated_at":"2021-10-20T23:14:32.817662Z"
+            },
+            {
+                "id":53,
+                "latitude":39.7754063,
+                "longitude":-105.0169859,
+                "message":"Big Love!",
+                "name":"Anonymous",
+                "title":"Hello",
+                "location":"",
+                "created_at":"2021-10-20T20:17:47.528244Z",
+                "updated_at":"2021-10-20T20:17:47.528270Z"}
+            ]
+        }
+    })
+
+})
 //
 //
 // -- This is a child command --
