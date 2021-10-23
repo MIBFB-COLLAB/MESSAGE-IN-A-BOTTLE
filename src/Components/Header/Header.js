41e85662704fde32cc31bd8822398e70296a68b2
Justin Anthony <justincanthony@gmail.com>
2021-10-22 21:50:45 -0600

    Wrap with Box component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,26 +13,28 @@ import { NewStoryForm } from '../NewStoryForm/NewStoryForm';
 const Header = () => {
   return (
     <header className="Header">
-      <Link to="/" className="header-text">
-        Welcome to Message in a Bottle
-      </Link>
-      <MicroModal
-        backdrop="static"
-        trigger={(open) => (
-          <div onClick={open}>
-            <button className="story-submit">Submit a story</button>
-          </div>
-        )}
-      >
-        {(close) => {
-          return (
-            <article>
-              <h3>Create Your Message</h3>
-              <NewStoryForm />
-            </article>
-          );
-        }}
-      </MicroModal>
+      <Box>
+        <Link to="/" className="header-text">
+          Welcome to Message in a Bottle
+        </Link>
+        <MicroModal
+          backdrop="static"
+          trigger={(open) => (
+            <div onClick={open}>
+              <button className="story-submit">Submit a story</button>
+            </div>
+          )}
+        >
+          {(close) => {
+            return (
+              <article>
+                <h3>Create Your Message</h3>
+                <NewStoryForm />
+              </article>
+            );
+          }}
+        </MicroModal>
+      </Box>
     </header>
   );
 };
