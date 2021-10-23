668467f7f3d13ffae4091986a34acf1dc65a21ed
Fara Akhatova <faraakhatova@gmail.com>
2021-10-22 18:45:08 -0600

    Add test for story card title

--- cypress/integration/StoriesPage_spec.js
+++ cypress/integration/StoriesPage_spec.js
@@ -14,13 +14,19 @@ describe('StoriesPage', () => {
         
     })
 
-    it('Should be able go to a new page and see title,instructions and button', () => {
+    it('Should be able go to a new page and see title, instructions and button', () => {
         cy.get('.Header')
         cy.get('.header-text').contains('Welcome to Message in a Bottle')
         .get('.story-submit')
         cy.get('article')
         .get('p').contains('Happy searching!')
         cy.get('.disclaimer').contains('**')
+    })
+
+    it('Should be able to see cards with title and buttons', () => {
+        cy.get('.stories-container')
+        cy.get('.story-card')
+        .get('.story-title') // should have value of title
     })
 })
 
