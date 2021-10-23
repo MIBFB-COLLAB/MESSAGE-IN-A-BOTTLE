4725198ef24bb507c576e5d42d64a81023fc6c7b
Justin Anthony <justincanthony@gmail.com>
2021-10-22 21:41:26 -0600

    Import components from mui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,38 @@
 import './Header.css';
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
 import MicroModal from 'react-micro-modal';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { NewStoryForm } from '../NewStoryForm/NewStoryForm';
 
 const Header = () => {
   return (
     <header className="Header">
-      <Link to='/' className='header-text'> 
-        Welcome to Message in a Bottle 
+      <Link to="/" className="header-text">
+        Welcome to Message in a Bottle
       </Link>
       <MicroModal
-        backdrop='static'
+        backdrop="static"
         trigger={(open) => (
           <div onClick={open}>
             <button className="story-submit">Submit a story</button>
           </div>
         )}
-      > 
-      {(close) => {
+      >
+        {(close) => {
           return (
             <article>
               <h3>Create Your Message</h3>
-              <NewStoryForm/>
+              <NewStoryForm />
             </article>
           );
         }}
       </MicroModal>
-        
     </header>
   );
 };
