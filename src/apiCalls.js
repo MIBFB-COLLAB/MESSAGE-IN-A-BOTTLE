2f01e0419364ed02449ecfb2b05def08c3a225bc
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 15:37:51 -0600

    fix error in APIcalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,7 +8,7 @@ export const getData = async (latitude, longitude) => {
 
 export const getDirections = async (id, latitude, longitude) => {
   const response = await fetch(
-    `https://message-in-a-bottle-api.herokuapp.com/api/v1/stories/:${id}/directions?lat=${latitude}&long=${longitude}`,
+    `https://message-in-a-bottle-api.herokuapp.com/api/v1/stories/${id}/directions?lat=${latitude}&long=${longitude}`,
     { mode: 'cors' }
   );
   return await response.json();
@@ -16,7 +16,7 @@ export const getDirections = async (id, latitude, longitude) => {
 
 export const getStory = async (id, latitude, longitude) => {
   const response = await fetch(
-    `https://message-in-a-bottle-api.herokuapp.com/api/v1/stories/:${id}?latitude=${latitude}&longitude=${longitude}`,
+    `https://message-in-a-bottle-api.herokuapp.com/api/v1/stories/${id}?latitude=${latitude}&longitude=${longitude}`,
     { mode: 'cors' }
   )
   return await response.json();
