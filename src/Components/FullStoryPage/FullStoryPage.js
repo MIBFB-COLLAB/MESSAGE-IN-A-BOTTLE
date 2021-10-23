a6e4e992efd1da386f7462aebca32366dac6aa54
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 17:45:52 -0600

    Add fullStoryPage component

---
+++ src/Components/FullStoryPage/FullStoryPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './FullStoryPage.css';
+
+export const FullStoryPage = () => {
+
+  return(
+    <div>I'm a story page!</div>
+  )
+}
+
+export default FullStoryPage;
