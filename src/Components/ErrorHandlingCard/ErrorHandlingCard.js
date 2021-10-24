fe2d503331fddd0995b021f086d2207dc5645444
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 20:25:23 -0600

    import Link

--- src/Components/ErrorHandlingCard/ErrorHandlingCard.js
+++ src/Components/ErrorHandlingCard/ErrorHandlingCard.js
@@ -1,5 +1,6 @@
 import './ErrorHandlingCard.css';
 import React from "react"
+import { Link } from 'react-router-dom'
 
 const ErrorHandlingCard = ({ errorMessage, networkMessage, directionMessage }) => {
   return (
