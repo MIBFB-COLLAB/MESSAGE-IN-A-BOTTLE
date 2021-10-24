3637eb754fd42f8bec6392b83ca5fff4cb77e65d
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 18:06:01 -0600

    Add full stories page route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import StoriesPage from '../StoriesPage/StoriesPage';
 import HomePage from '../HomePage/HomePage';
 import { Route, Switch } from 'react-router-dom';
 import ErrorHandlingPage from '../ErrorHandlingPage/ErrorHandlingPage';
+import FullStoryPage from '../FullStoryPage/FullStoryPage';
 
 const App = () => {
   return (
@@ -11,10 +12,19 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route
-          exact
-          path="/storiesPage/:latitude/:longitude"
+          exact path="/storiesPage/:latitude/:longitude"
           render={({ match }) => (
             <StoriesPage
+              latitude={match.params.latitude}
+              longitude={match.params.longitude}
+            />
+          )}
+        />
+        <Route 
+          exact path="/fullStoryPage/:id/:latitude/:longitude"
+          render={({ match }) => (
+            <FullStoryPage
+              id={match.params.id}
               latitude={match.params.latitude}
               longitude={match.params.longitude}
             />
