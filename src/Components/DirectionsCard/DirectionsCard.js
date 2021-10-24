18182c094ca76e94fd91ca2bc157bc03d0e9a39b
Justin Anthony <justincanthony@gmail.com>
2021-10-24 01:13:50 -0600

    Add mui components for directions list

--- src/Components/DirectionsCard/DirectionsCard.js
+++ src/Components/DirectionsCard/DirectionsCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 export const DirectionsCard = ({ direction }) => {
   const { narrative, distance } = direction;
+  const listItemStyle = {
+    borderBottom: 'solid #EAEDF2 3px',
+  };
+
   return (
-    <>
-      {
-        <article>
-          <p>
-            {narrative} for {distance}
-          </p>
-        </article>
-      }
-    </>
+    <ListItem sx={listItemStyle}>
+      <ListItemText
+        primary={narrative}
+        secondary={`For the next ${distance}`}
+      />
+    </ListItem>
   );
 };
