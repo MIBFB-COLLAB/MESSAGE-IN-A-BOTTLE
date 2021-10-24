b86f3789aae855724f83c6729866a262a2cb94ca
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 18:11:34 -0600

    add methods to full story page

--- src/Components/FullStoryPage/FullStoryPage.js
+++ src/Components/FullStoryPage/FullStoryPage.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './FullStoryPage.css';
 import { getStory } from '../../apiCalls';
 
-export const FullStoryPage = () => {
+export const FullStoryPage = ({ id, latitude, longitude }) => {
 
-  // const getSingleStory = () => {
-  //   getStory(id, latitude, longitude)
-  //   .then((data) => setStory(data.data.attributes))
-  //   .catch((error) => setError(error));
-  //   setIsLoading(false)
-  // };
+  const [story, setStory] = useState({})
+  const [error, setError] = useState('')
+
+  const getSingleStory = () => {
+    getStory(id, latitude, longitude)
+    .then((data) => setStory(data.data.attributes))
+    .catch((error) => setError(error));
+  };
+
+  useEffect(() => {
+    console.log('gettin a story')
+    getSingleStory()
+  }, [])
 
   return(
     <div>I'm a story page!</div>
