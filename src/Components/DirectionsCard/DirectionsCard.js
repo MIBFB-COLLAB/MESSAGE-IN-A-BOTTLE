f96394eb8a99b81e51fc00714736ceb270e2fb4c
Justin Anthony <justincanthony@gmail.com>
2021-10-23 23:41:37 -0600

    Fix logic for displaying directions

--- src/Components/DirectionsCard/DirectionsCard.js
+++ src/Components/DirectionsCard/DirectionsCard.js
@@ -4,14 +4,13 @@ export const DirectionsCard = ({ direction }) => {
   const { narrative, distance } = direction;
   return (
     <>
-      {distance > 0 && (
+      {
         <article>
           <p>
             {narrative} for {distance}
           </p>
         </article>
-      )}
-      {distance === '0 miles' && <p>"You have reached the story's origin"</p>}
+      }
     </>
   );
 };
