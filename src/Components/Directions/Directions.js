83786aec98aa36cf1f8234f46fef6489f35251fa
Justin Anthony <justincanthony@gmail.com>
2021-10-23 21:40:42 -0600

    Pass props down

--- src/Components/Directions/Directions.js
+++ src/Components/Directions/Directions.js
@@ -1,15 +1,14 @@
 import './Directions.css';
-import React from "react"
+import React from 'react';
 
-const Directions = ({title, directions}) => {
-  //directions will be an array of string we can iterate though to display
-  return(
+const Directions = ({ id, latitude, longitude }) => {
+  return (
     <article className="directions">
       <h3>{title}</h3>
       <p>{directions}</p>
       <button>BACK TO CARD</button>
     </article>
-  )
-}
+  );
+};
 
 export default Directions;
