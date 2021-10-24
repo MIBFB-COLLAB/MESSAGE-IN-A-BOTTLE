50b5bfc06277b0dbab3d8babb3653a517fe1d99d
Mae Duphorne <maelduphorne@gmail.com>
2021-10-23 18:06:57 -0600

    move getsinglestory to fullstorypage

--- src/Components/FullStoryPage/FullStoryPage.js
+++ src/Components/FullStoryPage/FullStoryPage.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import './FullStoryPage.css';
+import { getStory } from '../../apiCalls';
 
 export const FullStoryPage = () => {
+
+  // const getSingleStory = () => {
+  //   getStory(id, latitude, longitude)
+  //   .then((data) => setStory(data.data.attributes))
+  //   .catch((error) => setError(error));
+  //   setIsLoading(false)
+  // };
 
   return(
     <div>I'm a story page!</div>
