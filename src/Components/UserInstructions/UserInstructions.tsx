f171c8b1c41173ecb92d6ea54ae44b20f8ba3fad
Mae Duphorne <maelduphorne@gmail.com>
2021-10-24 21:42:43 -0600

    edit user instructions

--- src/Components/UserInstructions/UserInstructions.tsx
+++ src/Components/UserInstructions/UserInstructions.tsx
@@ -5,17 +5,16 @@ const UserInstructions = () => {
   return (
     <article className="instructions">
       <p>
-        Search your city for hidden treasures in story form. After sharing your
+        Search your city for hidden treasures in the form of messages. After sharing your
         current location, you will be taken to a list of messages in your area,
         their titles and the distance from your current location. To view
-        directions to a specific message's location, press the "VIEW DIRECTIONS"
-        button. Once you are within 1 mile of the message's location, you will
+        directions to a message's location, click the "GET DIRECTIONS"
+        button. Once you are within 1/4 mile of the message's location, you will
         be able to select the "VIEW STORY" button to unlock the content of the
         message. Happy searching!
       </p>
       <p className="disclaimer">
-        **please be advised that access to your current location is required for
-        full access**
+        **please be advised that access to your current location is required for full usability**
       </p>
     </article>
   );
