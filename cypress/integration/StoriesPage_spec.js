a5c942bf9063a0e1d109af9cad3e62a6d49d8f50
Mae Duphorne <maelduphorne@gmail.com>
2021-10-25 20:16:30 -0600

    Test directions button

--- cypress/integration/StoriesPage_spec.js
+++ cypress/integration/StoriesPage_spec.js
@@ -18,28 +18,20 @@ describe('StoriesPage', () => {
       .contains('TestTest');
   });
 
-  // it('Should be able to click button to get directions', () => {
-  //   cy.get('.stories-container');
-  //   cy.get('.MuiPaper-root')
-  //     .eq(0)
-  //     .get('h6')
-  //     .eq(0)
-  //     .get('.get-directions-btn')
-  //     .eq(0)
-  //     .click()
-  //     .wait(5000);
-    // cy.getDirections('GET');
-    // .visit('http://localhost:3000/directionsPage/39.6265535/-104.8108433')
-    // .url()
-    // .should('include', '/directionsPage/39.6265535/-104.8108433')
-    // .url()
-    // .should(
-    //   'eq',
-    //   'http://localhost:3000/directionsPage/39.6265535/-104.8108433'
-    // );
-  // });
+  it('Should be able to click button to get directions', () => {
+    cy.get('.stories-container');
+    cy.get('.MuiPaper-root')
+      .eq(0)
+      .get('h6')
+      .eq(0)
+      .get('.get-directions-btn')
+      .eq(0)
+      .click()
+      .wait(5000)
+    cy.get('.directions').should('be.visible')
+  });
 
-  //   it('Should be able to click button to view a story', () => {
-  //     cy.get('.view-story-btn'); // need click
-  //   });
+    // it('Should be able to click button to view a story', () => {
+    //   cy.get('.view-story-btn')
+    // });
 });
