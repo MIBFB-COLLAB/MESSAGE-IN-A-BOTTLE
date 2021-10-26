a4f70c3939bef9876f1f8d05bc1609abcd5826e1
Justin Anthony <justincanthony@gmail.com>
2021-10-26 00:05:42 -0600

    Adjust elements for user display

--- src/Components/UserInstructions/UserInstructions.tsx
+++ src/Components/UserInstructions/UserInstructions.tsx
@@ -3,18 +3,12 @@ import './UserInstructions.css';
 const UserInstructions = () => {
   return (
     <article className="instructions">
-      <p>
-        Search your city for hidden treasures in the form of messages. After sharing your
-        current location, you will be taken to a list of messages in your area,
-        their titles and the distance from your current location. To view
-        directions to a message's location, click the "GET DIRECTIONS"
-        button. Once you are within 1/4 mile of the message's location, you will
-        be able to select the "VIEW STORY" button to unlock the content of the
-        message. Happy searching!
-      </p>
-      <p className="disclaimer">
-        **please be advised that access to your current location is required for full usability**
-      </p>
+      <h4 className="user-instructions">
+        Search your city for hidden treasures in the form of messages. 
+      </h4>
+      <h6 className="disclaimer">
+        **please be advised that access to your current location is required for usability**
+      </h6>
     </article>
   );
 };
