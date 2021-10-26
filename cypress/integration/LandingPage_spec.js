d534386e76d34a5531a2436385fda665da0400fc
Justin Anthony <justincanthony@gmail.com>
2021-10-26 13:59:09 -0600

    Adjust for new components

--- cypress/integration/LandingPage_spec.js
+++ cypress/integration/LandingPage_spec.js
@@ -1,84 +1,82 @@
 describe('Landing Page', () => {
-  beforeEach('Home Page' , () => {
-    cy.visit('http://localhost:3000/')
+  beforeEach('Home Page', () => {
+    cy.visit('http://localhost:3000/');
   });
 
   it('Should be able to access to application by URL and see Home Page', () => {
-    cy.url().should('include', '/')
-    cy.url().should('eq', 'http://localhost:3000/')
-    
+    cy.url().should('include', '/');
+    cy.url().should('eq', 'http://localhost:3000/');
   });
 
   it('Should be able to show an error if URL path is wrong', () => {
-    cy.visit('http://localhost:3000/hotdogs')
-    cy.get('.error-message').contains('Whoops, something went wrong!')
-    cy.get('a > .MuiButton-root').click()
-    cy.url().should('eq', 'http://localhost:3000/')
-  })
+    cy.visit('http://localhost:3000/hotdogs');
+    cy.get('.error-message').contains('Whoops, something went wrong!');
+    cy.get('a > .MuiButton-root').click();
+    cy.url().should('eq', 'http://localhost:3000/');
+  });
 
   it('Should be able to see page Title', () => {
-    cy.get('Header')
-    cy.get('.header-text').contains('Welcome to Message in a Bottle')
+    cy.get('Header');
+    cy.get('.header-text').contains('Message in a Bottle');
   });
 
   it('Should be able to see instructions to follow', () => {
-    cy.get('article')
-    .get('p').contains('Happy searching!')
-    cy.get('.disclaimer').contains('**')
+    cy.get('article').get('h4').contains('Search your city');
+    cy.get('.disclaimer').contains('**');
   });
 
   it('Should be able to see a button to submit a story', () => {
-    cy.get('Header')
-    .get('div')
-    cy.get('.MuiButton-root')
-  })
-
-  it('Should be able click submit story button, in order to submit new story', () => {
-    cy.get('Header')
-    .get('div')
-    cy.get('.MuiButton-root').click()
-    cy.on('window:confirm',(txt)=>{
-      expect(txt).to.contains('Allow', 'Block');
-    })
+    cy.get('Header').get('div');
+    cy.get('.MuiButton-root');
   });
 
-  
+  it('Should be able click submit story button, in order to submit new story', () => {
+    cy.get('Header').get('div');
+    cy.get('.MuiButton-root').eq(0).click();
+    cy.on('window:confirm', (txt) => {
+      expect(txt).to.contains('Allow', 'Block');
+    });
+  });
+
   it('Should be able to get stories near by clicking the button', () => {
-    cy.get('.location-selection')
-    .get('.get-stories-btn').contains('Get Stories').click()
-  })
+    cy.get('.location-selection').get('.MuiLoadingButton-root').click();
+  });
 });
 
 describe('Create new story', () => {
   beforeEach('New card form modal', () => {
-    cy.visit('http://localhost:3000/')
-  
-  })
+    cy.visit('http://localhost:3000/');
+  });
 
   it('Should be able click submit story button, in order to submit new story', () => {
-    cy.get('Header')
-    .get('div')
-    cy.get('.MuiButton-root').click()
-    cy.on('window:confirm',(txt)=>{
+    cy.get('Header').get('div');
+    cy.get('.MuiButton-root').eq(0).click();
+    cy.on('window:confirm', (txt) => {
       expect(txt).to.contains('Allow', 'Block');
-    })
+    });
 
     cy.get('article')
-    .get('h3').contains('Create Your Message')
-    .get('div')
-    .get('#newStoryModal')
-    .get('.title').type('Story')
-    cy.get('[id="message"]').type('Paleo tacos cliche DIY, dreamcatcher trust fund iPhone taxidermy hammock chillwave bespoke artisan sustainable.')
-    .get('button').contains('Submit Story').click()
-    cy.PostStory('POST')
+      .get('h3')
+      .contains('Create Your Message')
+      .get('div')
+      .get('#newStoryModal')
+      .get('.title')
+      .type('Story');
+    cy.get('[id="message"]')
+      .type(
+        'Paleo tacos cliche DIY, dreamcatcher trust fund iPhone taxidermy hammock chillwave bespoke artisan sustainable.'
+      )
+      .get('button')
+      .contains('Submit Story')
+      .click();
+    cy.PostStory('POST');
   });
 
   it('Should be able to see button and text to get stories near by', () => {
-    cy.get('.location-selection')
-    .get('.get-stories-btn').contains('Get Stories').click()
-    cy.GetStory('GET')
-})
-})
+    cy.get('.location-selection').get('.MuiLoadingButton-root').click();
+    cy.GetStory('GET');
+  });
+});
 
 describe('EditStory component', () => {
   // beforeEach('Edit story card form modal', () => {
@@ -95,7 +93,7 @@ describe('EditStory component', () => {
 
   // it('After submitting new story, should be able to see new pop up modal with edit options and edit story', () => {
   //   cy.get('#newStoryModal').wait(5000)
-    // cy.get('#editInstructions').contains('Bottle')
+  // cy.get('#editInstructions').contains('Bottle')
   //   cy.get('#outlinedTextArea').type(' editing faras test')
   //   .get('#title').type('Test')
   //   .get('#text').contains('Once')
@@ -108,25 +106,33 @@ describe('EditStory component', () => {
   //   .get('#storyTitle')
   //   .get('#miles').contains('Miles')
   // })
-  
+
   it('Should be able to delete story by clicking button and modal has to close', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('.MuiButton-root').click()
+    cy.visit('http://localhost:3000/');
+    cy.get('.MuiButton-root').click();
     cy.get('article')
-    .get('h3').contains('Create Your Message')
-    .get('div')
-    .get('.title').type('Delete test')
-    .get('[id="message"]').type('faras delete test')
-    .get('button').contains('Submit Story').click()
-    cy.PostStory('POST')
-    cy.get('#newStoryModal')
-    cy.get('#editInstructions').contains('Bottle')
-    .get('#text').contains('Once')
-    .get('#deleteBtn').click()
-      cy.DeleteStory('DELETE')
-      cy.visit('http://localhost:3000/')
-      cy.get('.get-stories-btn').contains('Get Stories').click()
-      cy.get('.instructions').contains('location')
-      cy.expect('#storyTitle').to.not.equal('Delete test')
-  })
-})
+      .get('h3')
+      .contains('Create Your Message')
+      .get('div')
+      .get('.title')
+      .type('Delete test')
+      .get('[id="message"]')
+      .type('faras delete test')
+      .get('button')
+      .contains('Submit Story')
+      .click();
+    cy.PostStory('POST');
+    cy.get('#newStoryModal');
+    cy.get('#editInstructions')
+      .contains('Bottle')
+      .get('#text')
+      .contains('Once')
+      .get('#deleteBtn')
+      .click();
+    cy.DeleteStory('DELETE');
+    cy.visit('http://localhost:3000/');
+    cy.get('.get-stories-btn').contains('Get Stories').click();
+    cy.get('.instructions').contains('location');
+    cy.expect('#storyTitle').to.not.equal('Delete test');
+  });
+});
