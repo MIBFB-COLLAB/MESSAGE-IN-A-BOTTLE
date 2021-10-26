1c128bd75c3d2ca88c3eea156f2c70ea49f2de53
Justin Anthony <justincanthony@gmail.com>
2021-10-26 12:34:57 -0600

    Add classnames and elems

--- src/Components/StoriesPage/StoriesPage.tsx
+++ src/Components/StoriesPage/StoriesPage.tsx
@@ -4,10 +4,13 @@ import StoriesContainer from '../StoriesContainer/StoriesContainer';
 const StoriesPage = ({ latitude, longitude }) => {
   return (
     <main className="stories-page">
-      <p className="instructions">
-        Click a story's directions for help navigating to view the message. 
-        Click to view the story if you are within 1/4 mile of the story's location.
-      </p>
+      <div className="instructions">
+        <h4 className="user-instructions">
+          Click a story's directions for help navigating to view the message.</h4>
+        <h4 className="user-instructions">
+          Click to view the story if you are within 1/4 mile of the story's location.
+        </h4>
+      </div>
       <StoriesContainer latitude={latitude} longitude={longitude} />
     </main>
   );
